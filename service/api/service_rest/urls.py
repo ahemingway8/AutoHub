d7876f1fa128944cfca6cfe48acc8a1059b6c90d
EricW <ericjamesw@yahoo.com>
2024-12-12 10:17:53 -0800

    Second draft of views

---
+++ service/api/service_rest/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import technicians, technician, appointments, appointment
+
+
+urlpatterns = [
+    path("technicians/", technicians, name="technicians"),
+    path("technicians/<int:id>/", technician, name="technician"),
+    path('appointments/', appointments, name="appointments"),
+    path('appointments/<int:id>/', appointment, name="appointment"),
+    # path('appointments/<int:id>/cancel/', name="cancel"),
+    # path('appointments/<int:id>/finish/', name="finish"),
+]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,14 +1,72 @@
 from django.shortcuts import render
 import json
-from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 from .models import Technician, AutomobileVO, Appointment
+from .encoders import TechnicianEncoder, AppointmentEncoder
 
 @require_http_methods(["GET", "POST"])
-def list_tech(request):
+def technicians(request):
     if request.method == "GET":
         technicians = Technician.objects.all()
         return JsonResponse(
             {"technicians": technicians},
-            
+             encoder=TechnicianEncoder,
         )
+    else:
+        try:
+            content = json.loads(request.body)
+            technicians = Technician.objects.create(**content)
+            return JsonResponse(
+                technicians, encoder=TechnicianEncoder,
+                safe=False,
+            )
+        except:
+            response = JsonResponse(
+                {"message": "Could not submit technician"}
+            )
+            response.status_code = 400
+            return response
+
+def technician(request, id):
+    if request.method == "DELETE":
+        try:
+            technician = Technician.objects.get(id=id)
+            technician.delete()
+            return JsonResponse({"message": "Technician Deleted!"},
+                technician, encoder=TechnicianEncoder, safe=False,
+            )
+        except Technician.DoesNotExist:
+            return JsonResponse({"message": "Technician Does Not Exist"}, status=404)
+
+@require_http_methods(["GET", "POST"]) #DO SERVICE HISTORY WITH GET IF CAN
+def appointments(request):
+    if request.method == "GET":
+        appointments = Appointment.objects.all()
+        return JsonResponse(
+            {"appointments": appointments},
+            encoder=AppointmentEncoder,
+        )
+    else:
+        try:
+            content = json.loads(request.body)
+            appointment = Appointment.objects.create(**content)
+            return JsonResponse(
+                appointment, encoder=AppointmentEncoder,
+                safe=False,
+            )
+        except Appointment.DoesNotExist:
+            return JsonResponse({"message": "Appointment Does Not Exist"}, satus=404)
+
+def appointment(request, id):
+    if request.method == "DELETE":
+        try:
+            appointment = Appointment.objects.get(id=id)
+            appointment.delete()
+            return JsonResponse({"message": "Appointment Deleted!"}, appointment,encoder=AppointmentEncoder, safe=False,
+            )
+        except Appointment.DoesNotExist:
+            return JsonResponse({"MESSAGE": "Appointment Does Not Exist"}, status=404)
+
+# def appointment_cancel(request):
+# def appointment_finish(request):
