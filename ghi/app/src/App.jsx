6085fc4124c5d7ce5d815f2dd875a52666bc2ee1
EricW <ericjamesw@yahoo.com>
2024-12-12 15:58:28 -0800

    Manu list create model vehicle list

--- ghi/app/src/App.jsx
+++ ghi/app/src/App.jsx
@@ -3,6 +3,9 @@ import MainPage from "./MainPage";
 import Nav from "./Nav";
 import AutomobilesList from "./AutomobilesList"
 import SalespeopleList from "./SalespeopleList";
+import ManufacturerList from "./ManufacturerList";
+import VehicleModelsList from "./VehicleModelsList";
+import CreateModel from "./CreateModel";
 
 function App() {
 	return (
@@ -13,6 +16,9 @@ function App() {
 					<Route path="/" element={<MainPage />} />
 					<Route path="automobiles" element={<AutomobilesList />} />
 					<Route path="salespeople" element={<SalespeopleList/>}/>
+					<Route path="/manufacturers" element={<ManufacturerList />} />
+					<Route path="/vehiclemodels" element={<VehicleModelsList />} />
+					<Route path="/createmodel" element={<CreateModel />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
---
+++ ghi/app/src/VehicleModelsList.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+export default function VehicleModelsList() {
+    const [ models, setModels ] = useState([]);
+
+    async function getModels() {
+        const url = 'http://localhost:8100/api/models/';
+        const res = await fetch(url);
+        const { models } = await res.json();
+        setModels(models);
+    }
+
+    useEffect(() => {getModels()}, []);
+
+    return (
+        <>
+        <div>
+        <h1>Models</h1>
+        <table className="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Manufacturer</th>
+                    <th scope="col">Picture</th>
+                </tr>
+            </thead>
+            <tbody className="table-group-divider">
+                {models.map(model => (
+                <tr key={ model.id + model.href }>
+                    <td>{ model.name }</td>
+                    <td>{ model.manufacturer.name }</td>
+                    <td>
+                        <img src={ model.picture_url } className="img-thumbnail" width='195' height="195"/>
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+        </div>
+        </>
+    );
+}
