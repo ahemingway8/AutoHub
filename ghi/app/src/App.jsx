13fbd9666e084265a4a8439c7a0cfc9b5d4202cb
April <aprilhemingway00@gmail.com>
2024-12-12 13:08:21 -0700

    customers list

--- ghi/app/src/App.jsx
+++ ghi/app/src/App.jsx
@@ -4,6 +4,8 @@ import Nav from "./Nav";
 import AutomobilesList from "./AutomobilesList"
 import SalespeopleList from "./SalespeopleList";
 import SalespersonForm from "./SalespersonForm";
+import CustomersList from "./CustomersList";
+
 
 function App() {
 	return (
@@ -15,6 +17,7 @@ function App() {
 					<Route path="automobiles" element={<AutomobilesList />} />
 					<Route path="salespeople" element={<SalespeopleList/>}/>
 					<Route path="addSalesperson" element={<SalespersonForm/>}/>
+					<Route path="customers" element={<CustomersList/>}/>
 				</Routes>
 			</div>
 		</BrowserRouter>
---
+++ ghi/app/src/CustomersList.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+
+function CustomersList() {
+    const [customers, setCustomers] = useState([]);
+
+    const fetchCustomers = async () => {
+        const url = 'http://localhost:8090/api/customers/';
+
+        try {
+            const response = await fetch(url);
+
+            if (response.ok) {
+                const data = await response.json();
+                setCustomers(data.customers);
+            } else {
+                console.error('Error fetching data:', response.statusText);
+            }
+        } catch (error) {
+            console.error('fetch error', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchCustomers();
+    }, []);
+
+
+    return (
+        <div>
+            <h1>Customers</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Phone Number</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {customers.map(customer => (
+                        <tr key={ customer.id }>
+                            <td>{ customer.first_name }</td>
+                            <td>{ customer.last_name }</td>
+                            <td>{ customer.phone_number }</td>
+                            <td>{ customer.address }</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default CustomersList;
